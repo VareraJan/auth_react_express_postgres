621ae3c37c25e64a9abb6107472d31428ed3c923
Valera <vareraj@gmail.com>
2022-05-05 12:06:26 +0300

    add loader

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,20 @@ import React from 'react'
 import { BrowserRouter } from 'react-router-dom';
 import { useRoutes } from './routes';
 import { useAuth } from './hooks/auth.hook';
-import 'materialize-css'
 import { AuthContext } from './context/AuthContext';
 import NavBar from './components/NavBar';
+import Loader from './components/Loader';
+import 'materialize-css'
 
 function App() {
-  const {login, logout, token, userId} = useAuth()
+  const {login, logout, token, userId, ready} = useAuth()
   const isAuthenticated = !!token
   const routes = useRoutes(isAuthenticated)
+
+  if(!ready) {
+    return <Loader />
+  }
+
   return (
     <AuthContext.Provider value={{
       login, logout, token, userId, isAuthenticated
